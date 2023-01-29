0f5ae443051bec47c9eb12ed044ebd835c090a43
Raman456123 <103272817+Raman456123@users.noreply.github.com>
2023-01-29 12:17:10 +0530

    Update start_help.py

--- Adarsh/bot/plugins/start_help.py
+++ Adarsh/bot/plugins/start_help.py
@@ -14,135 +14,7 @@ db = Database(Var.DATABASE_URL, Var.SESSION_NAME)
 from pyrogram.types import ReplyKeyboardMarkup
 
  
-  @xbot.on_message(filters.command('start') & filters.private)
-async def _startfile(bot, update):
-    if update.text == '/start':
-        await update.reply_text(
-            f"I'm File-Sharing!\nYou can share any telegram files and get the sharing link using this bot!\n\n/help for more details...",
-            True, reply_markup=InlineKeyboardMarkup(START_BUTTONS))
-        return
-
-    if len(update.command) != 2:
-        return
-    code = update.command[1]
-    if '-' in code:
-        msg_id = code.split('-')[-1]
-        # due to new type of file_unique_id, it can contain "-" sign like "agadyruaas-puuo"
-        unique_id = '-'.join(code.split('-')[0:-1])
-
-        if not msg_id.isdigit():
-            return
-        try:  # If message not belong to media group raise exception
-            check_media_group = await bot.get_media_group(TRACK_CHANNEL, int(msg_id))
-            check = check_media_group[0]  # Because func return`s list obj
-        except Exception:
-            check = await bot.get_messages(TRACK_CHANNEL, int(msg_id))
-
-        if check.empty:
-            await update.reply_text('Error: [Message does not exist]\n/help for more details...')
-            return
-        if check.video:
-            unique_idx = check.video.file_unique_id
-        elif check.photo:
-            unique_idx = check.photo.file_unique_id
-        elif check.audio:
-            unique_idx = check.audio.file_unique_id
-        elif check.document:
-            unique_idx = check.document.file_unique_id
-        elif check.sticker:
-            unique_idx = check.sticker.file_unique_id
-        elif check.animation:
-            unique_idx = check.animation.file_unique_id
-        elif check.voice:
-            unique_idx = check.voice.file_unique_id
-        elif check.video_note:
-            unique_idx = check.video_note.file_unique_id
-        if unique_id != unique_idx.lower():
-            return
-        try:  # If message not belong to media group raise exception
-            await bot.copy_media_group(update.from_user.id, TRACK_CHANNEL, int(msg_id))
-        except Exception:
-            await check.copy(update.from_user.id)
-    else:
-        return
-
-
-# Help msg
-@xbot.on_message(filters.command('help') & filters.private)
-async def _help(bot, update):
-    await update.reply_text("Supported file types:\n\n- Video\n- Audio\n- Photo\n- Document\n- Sticker\n- GIF\n- Voice note\n- Video note\n\n If bot didn't respond, contact @xgorn", True)
-
-
-async def __reply(update, copied):
-    msg_id = copied.message_id
-    if copied.video:
-        unique_idx = copied.video.file_unique_id
-    elif copied.photo:
-        unique_idx = copied.photo.file_unique_id
-    elif copied.audio:
-        unique_idx = copied.audio.file_unique_id
-    elif copied.document:
-        unique_idx = copied.document.file_unique_id
-    elif copied.sticker:
-        unique_idx = copied.sticker.file_unique_id
-    elif copied.animation:
-        unique_idx = copied.animation.file_unique_id
-    elif copied.voice:
-        unique_idx = copied.voice.file_unique_id
-    elif copied.video_note:
-        unique_idx = copied.video_note.file_unique_id
-    else:
-        await copied.delete()
-        return
-
-    await update.reply_text(
-        'Here is Your Sharing Link:',
-        True,
-        reply_markup=InlineKeyboardMarkup([
-            [InlineKeyboardButton('Sharing Link',
-                                  url=f'https://t.me/{xbot_username}?start={unique_idx.lower()}-{str(msg_id)}')]
-        ])
-    )
-    await asyncio.sleep(0.5)  # Wait do to avoid 5 sec flood ban 
-
-# Store media_group
-media_group_id = 0
-@xbot.on_message(filters.media & filters.private & filters.media_group)
-async def _main_grop(bot, update):
-    global media_group_id
-    if OWNER_ID == 'all':
-        pass
-    elif int(OWNER_ID) == update.from_user.id:
-        pass
-    else:
-        return
-
-    if int(media_group_id) != int(update.media_group_id):
-        media_group_id = update.media_group_id
-        copied = (await bot.copy_media_group(TRACK_CHANNEL, update.from_user.id, update.message_id))[0]
-        await __reply(update, copied)
-
-    else:
-        # This handler catch EVERY message with [update.media_group_id] param
-        # So we should ignore next >1_media_group_id messages
-        return
-
-
-# Store file
-@xbot.on_message(filters.media & filters.private & ~filters.media_group)
-async def _main(bot, update):
-    if OWNER_ID == 'all':
-        pass
-    elif int(OWNER_ID) == update.from_user.id:
-        pass
-    else:
-        return
-
-    copied = await update.copy(TRACK_CHANNEL)
-    await __reply(update, copied)
-
-
-xbot.run()
+  
 
 @StreamBot.on_message(filters.command('start') & filters.private & ~filters.edited)
 async def start(b, m):
@@ -187,14 +59,14 @@ async def start(b, m):
                     disable_web_page_preview=True)
                 return
         await m.reply_text(
-            text="**👋 Hᴇʏ...!**\n\n<i>I'm Telegram Files Streaming Bot As Well Direct Links Generator</i>\n\n<i>Click On Help To Get More Information</i>\n\n<b><i><u>Warning 🚸</u></i></b>\n\n<b>🔞 Pron Contents Leads To Permanenet Ban You.</b>",
+            text="**👋 Hᴇʏ...!**\n\n<i>I'm Telegram Files Streaming Bot As Well Direct Links and file share link Generator</i>\n\n<i>Click On /Help To Get More Information</i>\n\n<b><i><u>Warning 🚸</u></i></b>\n\n<b>🔞 Porn Contents Leads To Permanenet Ban You.</b>",
             reply_markup=InlineKeyboardMarkup(
                 [[
                    InlineKeyboardButton('ʜᴇʟᴘ', callback_data='help'),
                    InlineKeyboardButton('ᴀʙᴏᴜᴛ', callback_data='about')
                 ],        
                 [InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇꜱ", url='https://t.me/uploaddisk'),
-                 InlineKeyboardButton("ʀᴇᴘᴏ", url='https://github.com/Selfie-bd/Filetolinkdcbot')]
+                 InlineKeyboardButton("24/7 Movie Group", url='https://t.me/+XnCNbZ23N5wzZTQ1')]
                 ]
             ),
             disable_web_page_preview=True
@@ -234,7 +106,112 @@ async def start(b, m):
                 return
 
         get_msg = await b.get_messages(chat_id=Var.BIN_CHANNEL, message_ids=int(usr_cmd))
+ if check.empty:
+            await update.reply_text('Error: [Message does not exist]\n/help for more details...')
+            return
+        if check.video:
+            unique_idx = check.video.file_unique_id
+        elif check.photo:
+            unique_idx = check.photo.file_unique_id
+        elif check.audio:
+            unique_idx = check.audio.file_unique_id
+        elif check.document:
+            unique_idx = check.document.file_unique_id
+        elif check.sticker:
+            unique_idx = check.sticker.file_unique_id
+        elif check.animation:
+            unique_idx = check.animation.file_unique_id
+        elif check.voice:
+            unique_idx = check.voice.file_unique_id
+        elif check.video_note:
+            unique_idx = check.video_note.file_unique_id
+        if unique_id != unique_idx.lower():
+            return
+        try:  # If message not belong to media group raise exception
+            await bot.copy_media_group(update.from_user.id, TRACK_CHANNEL, int(msg_id))
+        except Exception:
+            await check.copy(update.from_user.id)
+    else:
+        return
 
+     # Help msg
+@StreamBot.on_message(filters.command('help') & filters.private)
+async def _help(bot, update):
+    await update.reply_text("Supported file types:\n\n- Video\n- Audio\n- Photo\n- Document\n- Sticker\n- GIF\n- Voice note\n- Video note\n\n If bot didn't respond, contact @xgorn", True)
+
+
+async def __reply(update, copied):
+    msg_id = copied.message_id
+    if copied.video:
+        unique_idx = copied.video.file_unique_id
+    elif copied.photo:
+        unique_idx = copied.photo.file_unique_id
+    elif copied.audio:
+        unique_idx = copied.audio.file_unique_id
+    elif copied.document:
+        unique_idx = copied.document.file_unique_id
+    elif copied.sticker:
+        unique_idx = copied.sticker.file_unique_id
+    elif copied.animation:
+        unique_idx = copied.animation.file_unique_id
+    elif copied.voice:
+        unique_idx = copied.voice.file_unique_id
+    elif copied.video_note:
+        unique_idx = copied.video_note.file_unique_id
+    else:
+        await copied.delete()
+        return
+
+    await update.reply_text(
+        'Here is Your Sharing Link:',
+        True,
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton('Sharing Link',
+                                  url=f'https://t.me/{StreamBot_username}?start={unique_idx.lower()}-{str(msg_id)}')]
+        ])
+    )
+    await asyncio.sleep(0.5)  # Wait do to avoid 5 sec flood ban 
+
+# Store media_group
+media_group_id = 0
+@StreamBot.on_message(filters.media & filters.private & filters.media_group)
+async def _main_grop(bot, update):
+    global media_group_id
+    if OWNER_ID == 'all':
+        pass
+    elif int(OWNER_ID) == update.from_user.id:
+        pass
+    else:
+        return
+
+    if int(media_group_id) != int(update.media_group_id):
+        media_group_id = update.media_group_id
+        copied = (await bot.copy_media_group(TRACK_CHANNEL, update.from_user.id, update.message_id))[0]
+        await __reply(update, copied)
+
+    else:
+        # This handler catch EVERY message with [update.media_group_id] param
+        # So we should ignore next >1_media_group_id messages
+        return
+
+
+# Store file
+@StreamBot.on_message(filters.media & filters.private & ~filters.media_group)
+async def _main(bot, update):
+    if OWNER_ID == 'all':
+        pass
+    elif int(OWNER_ID) == update.from_user.id:
+        pass
+    else:
+        return
+
+    copied = await update.copy(TRACK_CHANNEL)
+    await __reply(update, copied)
+
+
+StreamBot.run()
+     
+     #file2link:-
         file_size = None
         if get_msg.video:
             file_size = f"{humanbytes(get_msg.video.file_size)}"
